6d7a8ba2951642d71b23d0b91c40cd8eed483e9a
Milton Carlos Katoo <mckatoo@gmail.com>
2017-05-25 11:52:00 -0300

    CONTROLE DE ACESSO - IMPLEMENTANDO BUSCA

--- src/app/controle-acesso/controle-acesso.component.ts
+++ src/app/controle-acesso/controle-acesso.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FirebaseListObservable, AngularFireDatabase, FirebaseObjectObservable } from 'angularfire2/database';
 import { AngularFireAuthModule } from 'angularfire2/auth';
 import { Title } from '@angular/platform-browser';
+import { Subject } from 'rxjs/Subject';
 
 @Component({
   selector: 'app-controle-acesso',
@@ -15,8 +16,13 @@ export class ControleAcessoComponent implements OnInit {
   count: number;
   pagina: number = 0;
 	porPagina: number;
-  alunos = [];
+  alunos;
   alunosTotal:FirebaseListObservable<any>;
+  _query: object = {
+      query: {
+        orderByChild: 'nome',
+      }
+    };
 
   constructor(private db: AngularFireDatabase, private title: Title) {
     if (this.porPagina == undefined) {
@@ -34,6 +40,28 @@ export class ControleAcessoComponent implements OnInit {
 		this.getAlunos();
 	}
 
+  search(_search:string) {
+    this._query = {
+      query: {
+        orderByChild: 'matricula',
+        equalTo: _search
+      }
+    };
+    this.alunos = [];
+    this.alunosTotal = this.db.list('/alunos',this._query);
+    this.count = Object.getOwnPropertyNames('alunosTotal').length;
+    for (let i = ( this.pagina * this.porPagina ); i < (this.pagina * this.porPagina + this.porPagina); i++) {
+      if (i >= this.count) {
+        break;
+      }
+      this.alunosTotal.subscribe(
+        dados => this.alunos.push(dados[i])
+      );
+    }
+    console.log(this.count);
+    console.log(this.alunosTotal);
+  }
+
   getAlunos(){
     this.db.object('/alunos').subscribe(
       dados => {
@@ -41,11 +69,7 @@ export class ControleAcessoComponent implements OnInit {
       }
     );
     this.alunos = [];
-    this.alunosTotal = this.db.list('/alunos',{
-      query: {
-        orderByChild: 'nome'
-      }
-    });
+    this.alunosTotal = this.db.list('/alunos',this._query);
 
     for (let i = ( this.pagina * this.porPagina ); i < (this.pagina * this.porPagina + this.porPagina); i++) {
       if (i >= this.count) {
