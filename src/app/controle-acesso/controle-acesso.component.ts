564df93b8c303ba788aadb70339c57b27ed5d3be
Milton Carlos Katoo <itapira.cpd@gmail.com>
2017-06-01 22:55:22 -0300

    BUSCA OK

--- src/app/controle-acesso/controle-acesso.component.ts
+++ src/app/controle-acesso/controle-acesso.component.ts
@@ -1,3 +1,4 @@
+import { Observable } from 'rxjs/Observable';
 import { Component, OnInit } from '@angular/core';
 import { FirebaseListObservable, AngularFireDatabase, FirebaseObjectObservable } from 'angularfire2/database';
 import { AngularFireAuthModule } from 'angularfire2/auth';
@@ -15,101 +16,100 @@ export class ControleAcessoComponent implements OnInit {
   editAluno: FirebaseListObservable<any>;
   display = 'hidden';
   count: number;
+  countTotal: number;
   pagina: number = 0;
 	porPagina: number;
   alunos = [];
-  // alunosTotal:FirebaseListObservable<any>;
-  alunosTotal;
-  _query: object = {
-    query: {
-      orderByChild: 'nome',
-    }
-  };
+  alunosTotal: FirebaseListObservable<any>;
 
   constructor(private db: AngularFireDatabase, private title: Title) {
     if (this.porPagina == undefined) {
       this.porPagina = 5;
     }
-    // this.getAlunos();
-    // const rootRef = firebase.database().ref();
-    // this.alunosTotal = rootRef.child('alunos').orderByChild('nome').startAt('ADAUTO').endAt('ADAUTO'+'\uf8ff');
-    // this.alunosTotal.on('value', snap => console.log(snap.val()));
-    this.db.list('/alunos',{
-      query: {
-        orderByChild: 'nome',
-        startAt: 'ADAUTO',
-        endAt: 'ADAUTO\uf8ff'
-      }
-    }).subscribe(dados => {
-      this.alunos = [];
-      for (let i in this.alunosTotal) {
-        if (this.alunosTotal.hasOwnProperty(i)) {
-          this.alunos.push(this.alunosTotal[i]);
-        }
-      }
-
-    });
-    console.log(this.alunosTotal);
+    this.getAlunos();
   }
 
   ngOnInit() {
     this.title.setTitle('IESI - Controle de Acesso');
   }
 
-  // paginar($event: any) {
-	// 	this.pagina = $event - 1;
-	// 	this.getAlunos();
-	// }
+  paginar($event: any) {
+		this.pagina = $event - 1;
+		this.getAlunos();
+	}
 
-  // search(_search:string) {
-  //   function isNumber(n) {
-  //     return !isNaN(parseFloat(n)) && isFinite(n);
-  //   }
-  //   this.alunos = [];
-  //   if (_search == '') {
-  //     this._query = {
-  //       query: {
-  //         orderByChild: 'nome',
-  //       }
-  //     };
-  //   } else if (isNumber(_search)) {
-  //     this._query = {
-  //       query: {
-  //         orderByChild: 'matricula',
-  //         equalTo: _search
-  //       }
-  //     };
-  //     console.log('é número');
-  //   } else {
-  //     console.log('não é número');
-  //   }
-  //   this.getAlunos();
-  // }
+  temNumero(str) {
+    if (str.replace(/[^0-9]/g,'') == '') {
+      return false;
+    } else {
+      return true;
+    }
+  }
 
-  // alunosArray() {
-  //   this.alunos = [];
-  //   for (let i = ( this.pagina * this.porPagina ); i < (this.pagina * this.porPagina + this.porPagina); i++) {
-  //     if (i >= this.count) {
-  //       break;
-  //     }
-  //     this.alunosTotal.subscribe(
-  //       dados => {
-  //         if (dados[i] != undefined) {
-  //           this.alunos.push(dados[i]);
-  //         }
-  //       }
-  //     );
-  //   }
-  // }
+  search(_search:string) {
+    if (_search == '') {
+      this.getAlunos();
+    } else {
 
-  // getAlunos(){
-  //   this.alunosTotal = this.db.list('/alunos',this._query);
-  //   this.alunosTotal.subscribe(
-  //     dados => {
-  //       this.count = dados.length;
-  //     }
-  //   );
-  //   this.alunosArray();
-  // }
+      if (this.temNumero(_search)==true) {
+        this.getAlunos({
+          query: {
+            orderByChild: 'matricula',
+            equalTo: _search
+          }
+        });
+        if (_search.length == 4) {
+          this.getAlunos({
+            query: {
+              orderByChild: 'matricula',
+              equalTo: '3101000'+_search
+            }
+          });
+        }
+      }
+
+      if (this.temNumero(_search)==false) {
+        this.getAlunos({
+          query: {
+            orderByChild: 'nome',
+            startAt: _search.toUpperCase(),
+            endAt: _search.toUpperCase()+'\uf8ff'
+          }
+        });
+      }
+
+    }
+  }
+
+  getAlunos(_query?:object){
+    this.alunos = [];
+    let tudo:boolean;
+    if (_query == undefined) {
+      tudo = true;
+      _query = {
+        query: {
+          orderByChild: 'nome',
+        }
+      };
+    }
+    this.alunosTotal = this.db.list('/alunos',_query);
+    this.alunosTotal.subscribe(
+      dados => {
+        this.count = dados.length;
+        if (tudo == true) {
+          this.countTotal = dados.length;
+        }
+        for (let i = ( this.pagina * this.porPagina ); i < (this.pagina * this.porPagina + this.porPagina); i++) {
+          if (i >= this.count) {
+            break;
+          }
+          if (dados[i] != undefined) {
+            this.alunos.push(dados[i]);
+          }
+        }
+      }
+    );
+    
+  }
 
 }
