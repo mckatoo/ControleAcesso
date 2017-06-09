d4d3e0af7756ef570ca014e6e4403cb59f513c22
Milton Carlos Katoo <mckatoo@gmail.com>
2017-06-08 23:06:27 -0300

    sdlfjs

--- src/app/configuracoes/usuarios/usuarios.component.ts
+++ src/app/configuracoes/usuarios/usuarios.component.ts
@@ -1,3 +1,4 @@
+import { AngularFireAuth } from 'angularfire2/auth';
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 import { Component, OnInit } from '@angular/core';
 import * as firebase from 'firebase';
@@ -9,19 +10,43 @@ import * as firebase from 'firebase';
 })
 export class UsuariosComponent implements OnInit {
 
-  users:any;
+  users: any;
+  display: string;
+  editUser: object;
 
   constructor(
-    private db: AngularFireDatabase
+    private db: AngularFireDatabase,
+    private afAuth: AngularFireAuth
   ) {
-    // db.list('/users').subscribe(users => {
-    //   this.users = users;
-    //   console.log(this.users);
-    // });
+    db.list('/users').subscribe(users => {
+      this.users = users;
+    });
   }
 
   ngOnInit() {
     
+  }
+
+  save(json:string) {
+    this.users.push(json);
+  }
+  
+  editar(usuario){
+    this.display = '';
+    this.editUser = usuario;
+  }
+
+  update(aluno) {
+    this.users.update(aluno.$key,aluno);
+    this.display = 'hidden';
+  }
+  
+  delete(key: string) {
+    if (key == 'undefined') {
+      this.users.remove();
+    } else {
+      this.users.remove(key); 
+    }
   }
 
 }
